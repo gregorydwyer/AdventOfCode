82b5dd8bdbfd5452251519ed59cd0acee55c39b9
Gregory Dwyer <gdwyer@altair.com>
2025-12-04 13:03:02 -0500

    Add Day 3 and 4 solutions

--- AdventOfCode2025/AventOfCode2025/Day03.cs
+++ AdventOfCode2025/AventOfCode2025/Day03.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics.PerformanceData;
+using System.Dynamic;
+using System.Security.Cryptography;
 
 namespace AdventOfCode2024
 {
@@ -10,26 +13,79 @@ namespace AdventOfCode2024
 
         public static void Run()
         {
+            Program.WriteTitle("--- Day 3: Lobby ---");
             Problem1();
             Problem2();
         }
 
         public static void Problem1()
         {
-            Console.WriteLine(Day + " P1");
-            using (Program.GetReader(FileLocation))
+            Program.WriteProblemNumber("Part One");
+            using (var sr = Program.GetReader(FileLocation))
             {
+                var line = sr.ReadLine();
+                var total = 0L;
+                do
+                {
+                    var lmax = '0';
+                    var rmax = '0';
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (i < line.Length - 1
+                            && line[i] > lmax)
+                        {
+                            lmax = line[i];
+                            rmax = line[i + 1];
+                        }
+                        else if (line[i] > rmax)
+                        {
+                            rmax = line[i];
+                        }
+                    }
 
+                    total += int.Parse("" + lmax + rmax);
+                    line = sr.ReadLine();
+                } while (!string.IsNullOrEmpty(line));
+                Program.WriteOutput("2-Battery Joltage Potential: " + total);
             }
         }
 
         public static void Problem2()
         {
-            Console.WriteLine(Day + " P2");
-            using (Program.GetReader(FileLocation))
+            Program.WriteProblemNumber("Part Two");
+            using (var sr = Program.GetReader(FileLocation))
             {
-
+                var line = sr.ReadLine();
+                var total = 0L;
+                do
+                {
+                    var temp = GetHighestStartingFromIndex(line, 0, "");
+                    total += long.Parse(GetHighestStartingFromIndex(line,0,""));
+                    line = sr.ReadLine();
+                } while (!string.IsNullOrEmpty(line));
+                Program.WriteOutput("12-Battery Joltage Potential: " + total);
             }
+        }
+
+        private static string GetHighestStartingFromIndex(string str, int index, string current)
+        {
+            if (current.Length == 12)
+            {
+                return current;
+            }
+
+            for (char num = '9'; num >= '0'; num--)
+            {
+                var nexti = str.IndexOf(num, index);
+                if (nexti > -1
+                    && str.Length - nexti >= 12 - current.Length)
+                {
+                    current += num;
+                    return GetHighestStartingFromIndex(str, nexti + 1, current);
+                }
+            }
+            Program.WriteOutput("Whoops, We shouldn't have gotten here!");
+            throw new Exception();
         }
     }
 }
--- AdventOfCode2025/AventOfCode2025/Day04.cs
+++ AdventOfCode2025/AventOfCode2025/Day04.cs
@@ -1,35 +1,109 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode2024
 {
     public class Day04
     {
         private const string Day = "Day04";
+        private const char Roll = '@';
         private static readonly string FileLocation = Day + ".txt";
         private static readonly string TestFileLocation = Day + "test.txt";
 
         public static void Run()
         {
+            Program.WriteTitle("--- Day 4: Printing Department ---");
             Problem1();
             Problem2();
         }
 
         public static void Problem1()
         {
-            Console.WriteLine(Day + " P1");
-            using (Program.GetReader(FileLocation))
+            Program.WriteProblemNumber("Part One");
+            var grid = BuildGrid();
+            var count = 0;
+            for (int r = 0; r < grid.Count; r++)
             {
-
+                for (int c = 0; c < grid[r].Count; c++)
+                {
+                    if (grid[r][c] == Roll
+                        && CountAdjacentRolls(r, c, grid) < 4)
+                    {
+                        count++;
+                    }
+                }
             }
+
+            Program.WriteOutput("Movable rolls: " + count);
         }
 
         public static void Problem2()
         {
-            Console.WriteLine(Day + " P2");
-            using (Program.GetReader(FileLocation))
+            Program.WriteProblemNumber("Part Two");
+            var grid = BuildGrid();
+            var count = 0;
+            var rollsRemoved = false;
+            do
             {
+                rollsRemoved = false;
+                for (int r = 0; r < grid.Count; r++)
+                {
+                    for (int c = 0; c < grid[r].Count; c++)
+                    {
+                        if (grid[r][c] == Roll
+                            && CountAdjacentRolls(r, c, grid) < 4)
+                        {
+                            grid[r][c] = '.';
+                            rollsRemoved = true;
+                            count++;
+                        }
+                    }
+                }
+            } while (rollsRemoved);
+            Program.WriteOutput("Rolls Removed: " + count);
+        }
 
+        private static List<List<char>> BuildGrid()
+        {
+            using(var sr = Program.GetReader(FileLocation))
+            {
+                var grid = new List<List<char>>();
+                var line = sr.ReadLine();
+                do
+                {
+                    grid.Add(line.ToCharArray().ToList());
+                    line = sr.ReadLine();
+                } while (!string.IsNullOrEmpty(line));
+
+                return grid;
             }
         }
+
+        private static int CountAdjacentRolls(int row, int col, List<List<char>> grid)
+        {
+            var count = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    try
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            continue;
+                        }
+                        if (grid[row + i][col + j] == Roll)
+                        {
+                            count++;
+                        }
+                    }
+                    catch{}
+                }
+            }
+
+            return count;
+        }
+
     }
 }
